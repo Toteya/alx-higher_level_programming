1b9048da4c63e0d151f1028cc4cf12ce99b5a3d8
Toteya <111172685+Toteya@users.noreply.github.com>
2023-06-05 16:06:58 +0000

    Task10: Check if a singly-linked list has a cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * check_cycle - Checks if a singly-linked list has a cycle
+ * @list: The list to be checked
+ *
+ * Return: 0 if there is no cycle, and 1 if there is a cycle.
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *mark;
+
+	mark = list;
+
+	while (list)
+	{
+		list = list->next;
+		if (mark == list)
+			return (1);
+	}
+	return (0);
+}
