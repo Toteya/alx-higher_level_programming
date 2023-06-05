1cae76a8898a03f6da5d084d4fcc57bdf90071ee
Toteya <111172685+Toteya@users.noreply.github.com>
2023-06-05 17:35:23 +0000

    Task10: fixed for case: cycle that doesn't loop through the entire list

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -3,6 +3,8 @@
 
 #include <stdlib.h>
 
+/* Data Structures */
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -16,6 +18,21 @@ typedef struct listint_s
 	int n;
 	struct listint_s *next;
 } listint_t;
+
+/**
+ * struct listaddr_s - singly-linked list
+ * @val: Address of a list
+ * @next: Pointer to the next node
+ *
+ * Description: Stores the unique addresses of the nodes in a listint_t list.
+ */
+typedef struct listaddr_s
+{
+	listint_t *val;
+	struct listaddr_s *next;
+} listaddr_t;
+
+/* Function Prototypes */
 
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
