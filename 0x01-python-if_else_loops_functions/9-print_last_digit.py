106833cb011b7964b28dc4c946f7bc68ff96e7ea
Toteya <111172685+Toteya@users.noreply.github.com>
2023-06-06 18:58:10 +0000

    Task9: Fixed: Correct data type

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # Prints the last digit of a number
 def print_last_digit(number):
-    a = str(number)
+    a = str(int(number))
     b = a[-1]
     print("{}".format(b), end="")
     return b
