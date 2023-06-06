e1d0528e05f6354d17f3741856ed648ee2079c41
Toteya <111172685+Toteya@users.noreply.github.com>
2023-06-06 13:52:15 +0000

    Task1: Fixed output for strange modulo behaviour for negative numbers in Python

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 import random
+import math
 number = random.randint(-10000, 10000)
-ld = abs(number) % 10
+ld = math.fmod(number, 10)
 if ld > 5:
     a = "greater than 5"
 elif ld == 0:
     a = "0"
+    ld = 0
 else:
     a = "less than 6 and not 0"
-print(f"The last digit of {number:d} is {ld} and is {a}")
+print(f"The last digit of {number:d} is {ld:.0f} and is {a}")
