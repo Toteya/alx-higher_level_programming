1fd417565a957cac633f82bec6d904f390fcf5de
Toteya <111172685+Toteya@users.noreply.github.com>
2023-06-06 18:42:03 +0000

    Task8: Print a string in uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Prints a string in uppercase
+def uppercase(str):
+    str_copy = ""
+    for c in str:
+        if c.islower():
+            str_copy = f"{str_copy}{(chr(ord(c) - 32))}"
+        else:
+            str_copy = f"{str_copy}{c}"
+    print(str_copy)
