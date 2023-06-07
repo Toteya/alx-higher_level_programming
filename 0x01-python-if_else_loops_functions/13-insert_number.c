bdf43f5d57ad3d13f250743cf4ebda92666d01cf
Toteya <111172685+Toteya@users.noreply.github.com>
2023-06-07 17:43:07 +0000

    Task13: Insert a node

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+ * insert_node - Inserts a number into a sorted singly-linked list listint_t
+ * @head: Double pointer to the first of the list
+ * @number: Number value of the new node
+ *
+ * Return: Address if the new node. Otherwise returns NULL if failed.
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new, *curr = NULL, *prev = NULL;
+
+	new = malloc(sizeof(listint_t));
+	if (!new || !head)
+		return (NULL);
+
+	new->n = number;
+	new->next = NULL;
+
+	if (!(*head))
+		*head = new;
+
+	curr = *head;
+	while (curr)
+	{
+		if (curr->n > new->n)
+		{
+			new->next = curr;
+			if (!prev)
+				*head = new;
+			else
+				prev->next = new;
+			new->next = curr;
+			return (new);
+		}
+		prev = curr;
+		curr = curr->next;
+	}
+	prev->next = new;
+	return (new);
+}
