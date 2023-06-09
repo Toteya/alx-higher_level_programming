11b227890196eea62fae90955b931325209bbc25
Toteya <111172685+Toteya@users.noreply.github.com>
2023-06-09 18:26:31 +0000

    Task0: Print all the integers of a list, fixed: format

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for x in my_list:
-        print("{}".format(x))
+        print("{:d}".format(x))
     # Alternative Approach:
     # print(*my_list, sep="\n")
