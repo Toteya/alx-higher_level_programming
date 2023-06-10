527529892f0d7581fdd706becfc5cab345a5e92c
Toteya <111172685+Toteya@users.noreply.github.com>
2023-06-10 15:38:09 +0000

    Palindrome: Less loops but more mallocs

---
+++ 0x03-python-data_structures/13-is_palindrome_v1.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - Checks if a singly-linked list listint_t is a palindrome
+ * @head: Double pointer to the first node of the list
+ *
+ * Return: 1 if the list is a palindrome. Otherwise return 0.
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t **ptr;
+	unsigned int i = 0, j;
+
+	if (!(*head) || (*head)->next == NULL)
+		return (1);
+
+	ptr = malloc(sizeof(listint_t *));
+	if (!ptr)
+		exit(EXIT_FAILURE);
+
+	*ptr = *head;
+
+	while (ptr[i]->next)
+	{
+		i++;
+		ptr = realloc(ptr, sizeof(listint_t *) * (i + 1));
+		if (!ptr)
+			exit(EXIT_FAILURE);
+		ptr[i] = ptr[i - 1]->next;
+	}
+	j = i;
+	for (i = 0; i < j; i++, j--)
+	{
+		if (ptr[i]->n != ptr[j]->n)
+		{
+			free(ptr);
+			return (0);
+		}
+	}
+	free(ptr);
+	return (1);
+}
