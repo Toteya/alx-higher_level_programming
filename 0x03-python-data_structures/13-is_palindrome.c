5c2ea65f3eaa642ed1b074c0f0e5195313c85f82
Toteya <111172685+Toteya@users.noreply.github.com>
2023-06-12 16:52:48 +0000

    Palindrome: Halved memory allocation

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -9,7 +9,8 @@
 int is_palindrome(listint_t **head)
 {
 	listint_t **ptr, *p;
-	unsigned int i = 0, j;
+	unsigned int i = 0, n;
+	int even;
 
 	if (!(*head) || (*head)->next == NULL)
 		return (1);
@@ -21,26 +22,28 @@ int is_palindrome(listint_t **head)
 		i++;
 	}
 
-	ptr = malloc(sizeof(listint_t *) * (i + 1));
+	even = i % 2;
+	n = i /  2;
+	ptr = malloc(sizeof(listint_t *) * (n + 1));
 	if (!ptr)
 		exit(EXIT_FAILURE);
 
 	*ptr = *head;
-	i = 0;
-	while (ptr[i]->next)
+	for (i = 0; i < n; i++)
 	{
 		ptr[i + 1] = ptr[i]->next;
-		i++;
 	}
 
-	j = i;
-	for (i = 0; i < j; i++, j--)
+	p = ptr[i + even];
+	while (p)
 	{
-		if (ptr[i]->n != ptr[j]->n)
+		if (ptr[i]->n != p->n)
 		{
 			free(ptr);
 			return (0);
 		}
+		p = p->next;
+		i--;
 	}
 	free(ptr);
 	return (1);
