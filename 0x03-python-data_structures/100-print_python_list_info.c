2be2cca381ec522a86222c3ae075f2ce0ce6f06a
Toteya <111172685+Toteya@users.noreply.github.com>
2023-06-13 12:19:49 +0000

    Task14: task files

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <listobject.h>
+#include <object.h>
+
+/**
+ * print_python_list_info - Print some basic info about Python lists (PyObject)
+ * p - Pointer to the PyObject 
+ *
+ * Return: Nothing.
+ */
+void print_python_list_info(PyObject *p)
+{
+	;
+}
