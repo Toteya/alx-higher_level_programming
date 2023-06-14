79f3a87ca24b474def34ca90cf5034046cbe3fe1
Toteya <111172685+Toteya@users.noreply.github.com>
2023-06-14 19:05:31 +0000

    Search and replace: Simplified code

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = my_list.copy()
-    done = False
-    while (not done):
-        try:
-            new_list.insert(new_list.index(search), replace)
-            new_list.remove(search)
-        except ValueError:
-            done = True
+    while (search in new_list):
+        new_list.insert(new_list.index(search), replace)
+        new_list.remove(search)
     return new_list
