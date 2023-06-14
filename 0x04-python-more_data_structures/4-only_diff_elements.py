402a4414bbc11d9eee3d97dcb8cb26e3b63a2163
Toteya <111172685+Toteya@users.noreply.github.com>
2023-06-14 18:12:40 +0000

    Task4: set symmetric difference

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,10 @@
+#!/usr/bin/pyhton3
+def only_diff_elements(set_1, set_2):
+    sym_diff = set()
+    if not isinstance(set_1, set):
+        sym_diff = set_2.copy()
+    elif not isinstance(set_2, set):
+        sym_diff = set_1.copy()
+    else:
+        sym_diff = set_1 ^ set_2
+    return sym_diff
