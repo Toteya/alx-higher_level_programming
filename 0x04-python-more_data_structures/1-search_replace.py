93c189781c7d4812a1324f4efcad3814dec0695b
Toteya <111172685+Toteya@users.noreply.github.com>
2023-06-14 19:07:34 +0000

    Search and replace: Simplified code

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = my_list.copy()
-    while (search in new_list):
+    while search in new_list:
         new_list.insert(new_list.index(search), replace)
         new_list.remove(search)
     return new_list
