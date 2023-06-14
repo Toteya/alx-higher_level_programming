de072378b47bd4d673b95440f100ed1c3a4b387d
Toteya <111172685+Toteya@users.noreply.github.com>
2023-06-14 20:53:41 +0000

    Task10: Find the highest value in a dictionary, fixed - return key not value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        return max(list(a_dictionary.values()))
+        values = list(a_dictionary.values())
+        keys = list(a_dictionary.keys())
+        top_score = max(values)
+        return keys[values.index(top_score)]
