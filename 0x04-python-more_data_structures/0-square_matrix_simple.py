fe02b9369b01a207f7a1e74bf7e7cc2e3ccc9ef0
Toteya <111172685+Toteya@users.noreply.github.com>
2023-06-14 16:45:13 +0000

    Task0: Square a matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    square_matrix = []
+    if matrix:
+        for row in matrix:
+            square_matrix.append([x**2 for x in row])
+    return square_matrix
