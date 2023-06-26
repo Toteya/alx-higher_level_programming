c572c90315937a7d9d5952965543cf81d38c8cb1
Toteya <111172685+Toteya@users.noreply.github.com>
2023-06-26 20:45:49 +0000

    Task7: Correct output - Print err message to stderr

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
     except (ValueError, TypeError) as err:
-        print(str(err))
+        err_message = f"Exception: {str(err)}\n"
+        sys.stderr.write(str(err_message))
         return False
