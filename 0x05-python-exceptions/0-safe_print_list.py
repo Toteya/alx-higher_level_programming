73d7151851fcf7486358c98ad36fcf4f9fc4406a
Toteya <111172685+Toteya@users.noreply.github.com>
2023-06-26 16:28:07 +0000

    Safe print list: case - x=0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    elements = 0
     try:
         for i in range(x):
             print(my_list[i], end="")
+            elements += 1
     except IndexError:
-        i -= 1
+        pass
     print()
-    return i + 1
+    return elements
