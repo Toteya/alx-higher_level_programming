9a5d99a5915557107e582637a509b7e4141d5d4e
Toteya <111172685+Toteya@users.noreply.github.com>
2023-06-27 15:30:24 +0000

    Task2: Tried some fixes

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -11,9 +11,9 @@ class Square():
         Args:
             size (int): The initial size of the Square instance; must be >= 0
         """
-        if not isinstance(size, int):
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
-            raise ValueError("size must be >=0")
+            raise ValueError("size must be >= 0")
         else:
             self.__size = size
