4a01a9ccaeee7c4f89b9b66b425885fe553ed4d5
Toteya <111172685+Toteya@users.noreply.github.com>
2023-06-27 19:17:01 +0000

    Task4: Getter and setter properties for Square

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""This module defines the Square class"""
+
+
+class Square():
+    """This class defines a square and its attributes"""
+
+    def __init__(self, size=0):
+        """Initialises an instance of Square
+
+        Args:
+            size (int): The initial size of the Square instance; must be >= 0
+        """
+        self.size = size
+
+    def area(self):
+        """Returns the area of the square (size^2)"""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """Getter method for the __size attribute"""
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """Setter method for the __size attribute"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
