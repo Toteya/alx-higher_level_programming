01c1bdb08dcbda59a723e6deeb039ef4086e9269
Toteya <111172685+Toteya@users.noreply.github.com>
2023-06-27 14:58:42 +0000

    Task1: Define a Square class and __init__

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module defines the Square class"""
+
+
+class Square():
+    """This class defines a square and its attributes"""
+
+    def __init__(self, size):
+        """Initialises an instance of Square
+
+        Args:
+            size: The initial size of the Square instance
+        """
+        self.__size = size
