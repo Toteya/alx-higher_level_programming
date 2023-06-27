4719ea9d91bd8401989e4743f8c77b8fd0cd72fd
Toteya <111172685+Toteya@users.noreply.github.com>
2023-06-27 18:51:42 +0000

    public instance method that returns the area of a square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""This module defines the Square class"""
+
+
+class Square():
+    """This class defines a square and its attributes"""
+
+    def __init__(self, size=0):
+        """Initialises an instance of Square
+
+        Args:
+            size (int): The initial size of the Square instance; must be >= 0
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Returns the area of the square (size^2)"""
+        return self.__size ** 2
