2dcdf6909ddfc1322b885db6a1193c14bb506767
Toteya <111172685+Toteya@users.noreply.github.com>
2023-07-03 10:16:21 +0000

    class Rectangle intance attributes

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" 0-rectangle module """
+
+
+class Rectangle:
+    """ Class Rectangle defines a rectangle """
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ The width of the rectangle
+
+        width must be an int, otherwise TypeError is raised
+        width must be >= 0
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+
+    @property
+    def height(self):
+        """ The height of the rectangle
+
+        height must be an integer, and must be >= 0
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
