2ef19486101b401a77f293615632be2d07fdeae7
Toteya <111172685+Toteya@users.noreply.github.com>
2023-07-04 10:22:55 +0000

    Task19: Copy a list

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
-""" Module 19-copy_list """
-
-
 def copy_list(l):
-    """ Returns a clone of a list
-
-    Args:
-        my_list (obj: `list`): The list to be cloned / copied
-    """
     return list(l)
