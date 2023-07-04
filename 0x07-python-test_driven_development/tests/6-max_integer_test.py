3477aea2e5d58d15192ef35289462c6b685b5d53
Toteya <111172685+Toteya@users.noreply.github.com>
2023-07-04 12:37:56 +0000

    Unittest a function

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    """ Tests max_integer function
+    """
+
+    def test_max_integer(self):
+        """ Tests cases that should pass
+        """
+        result = max_integer([2, 6, 8, 4])
+        self.assertEqual(result, 8)
+
+        result = max_integer([2, 6, -8, 4])
+        self.assertEqual(result, 6)
+
+        result = max_integer([-2, 0, -8])
+        self.assertEqual(result, 0)
+
+        result = max_integer([3])
+        self.assertEqual(result, 3)
+
+        result = max_integer([])
+        self.assertEqual(result, None)
