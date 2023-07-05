05f0bb0236e4d70b4f58bf6d7fc0a69134468097
Toteya <111172685+Toteya@users.noreply.github.com>
2023-07-05 19:43:10 +0000

    Matrix multiplication using numpy

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,9 @@
+""" Module `101-lazy_matrix_mul`
+contains function `lazy_matrix_mul()`
+"""
+import numpy as np
+
+def lazy_matrix_mul(m_a, m_b):
+    """ Multiplies two matrices """
+    return np.matmul(m_a, m_b)
+
