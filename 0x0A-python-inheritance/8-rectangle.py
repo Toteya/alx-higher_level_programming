2f5d3c692b33362e93a2b65bbd701ecc661ddd32
Toteya <111172685+Toteya@users.noreply.github.com>
2023-07-10 16:08:23 +0000

    Task8: new Rectangle class, subclass BaseGeometry

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,14 @@
+""" Module 8-rectangle """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ Defines a Rectangle
+    subclass of BaseGeometry
+    """
+
+    def __init__(self, width, height):
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
