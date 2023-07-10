4ab54606aff062503532ddc283f67078c34dde6d
Toteya <111172685+Toteya@users.noreply.github.com>
2023-07-10 13:00:24 +0000

    Task2: better code

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -5,4 +5,5 @@
 def is_same_class(obj, a_class):
     """ Returns true if obj is exactly an instance of a_class
     """
-    return obj.__class__ == a_class
+    # return obj.__class__ == a_class
+    return type(obj) is a_class
