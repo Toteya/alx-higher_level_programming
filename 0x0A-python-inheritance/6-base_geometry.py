5efd15a27db30f060e69f039e1c39ccaa453dadd
Toteya <111172685+Toteya@users.noreply.github.com>
2023-07-10 14:06:15 +0000

    Task6: BaseGeometry, area method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Module 6-base_geometry
+Contains empty class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """ Defines a Geometry object """
+
+    def area(self):
+        """ returns the area """
+        raise Exception("area() is not implemented")
