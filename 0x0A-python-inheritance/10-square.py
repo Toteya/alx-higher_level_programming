f50a7bf3c095c675484fa13665f63022fa506ae0
Toteya <111172685+Toteya@users.noreply.github.com>
2023-07-10 17:42:30 +0000

    Task 10: Make a Square from a Rectangle

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Module 10-square """
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Defines a Square object/shape
+    inherits Rectangle class
+    """
+
+    def __init__(self, size):
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """ Returns the area of the Square instance """
+        return self.__size ** 2
