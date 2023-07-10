f72c3175fa460a56a750aade03791968ed545565
Toteya <111172685+Toteya@users.noreply.github.com>
2023-07-10 13:36:28 +0000

    Task4: Check if a class is a subclass of another

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,4 +4,6 @@
 
 def inherits_from(obj, a_class):
     """ Returns True if the class of obj inherits from a_class """
-    return issubclass(obj.__class__, a_class)
+    if not isinstance(a_class, type):
+        return False
+    return type(obj) is not a_class and issubclass(obj.__class__, a_class)
