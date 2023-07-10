9b69c3123e4afbd0e1eee93a3106a9b38e27d574
Toteya <111172685+Toteya@users.noreply.github.com>
2023-07-10 12:52:10 +0000

    Task2: check if object is an exact instance of a class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Module 2-is_same_class """
+
+
+def is_same_class(obj, a_class):
+    """ Returns true if obj is exactly an instance of a_class
+    """
+    return obj.__class__ == a_class
