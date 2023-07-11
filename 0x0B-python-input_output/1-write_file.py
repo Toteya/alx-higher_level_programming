0ba004fff3ba41989d6d6cd70f24cea7d721c1ba
Toteya <111172685+Toteya@users.noreply.github.com>
2023-07-11 15:37:08 +0000

    Task1: Write text to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" 1-write_file module """
+
+
+def write_file(filename="", text=""):
+    with open(filename, "w", encoding="utf-8") as a_file:
+        return a_file.write(text)
