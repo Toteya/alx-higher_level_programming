469c32aed58f6067756f111d1aba4d919221bb31
Toteya <111172685+Toteya@users.noreply.github.com>
2023-07-11 15:04:28 +0000

    Read and print a text file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+""" Module 0-read_file.py """
+
+
+def read_file(filename=""):
+    """ Reads the contents of a file and prints to stdout
+    """
+    with open(filename, encoding="utf-8") as a_file:
+        text = a_file.read()
+        print(text, end="")
