1684f3f680946442b7c1c93fc429f755de782d4c
Toteya <111172685+Toteya@users.noreply.github.com>
2023-07-11 21:04:36 +0000

    Python script that adds cmd line arguments to Python list, fixed filename

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,7 +4,7 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-filename = "add_file.json"
+filename = "add_item.json"
 try:
     a_list = load_from_json_file(filename)
 except FileNotFoundError:
