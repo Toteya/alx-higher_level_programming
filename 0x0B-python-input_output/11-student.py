15a8ce55279e6c6c90ea2167d309deec77266db1
Toteya <111172685+Toteya@users.noreply.github.com>
2023-07-12 14:14:58 +0000

    Task11:

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Module 11-student """
+
+
+class Student:
+    """ class Student """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Returns a dictionary representation of this
+        Student instance of the given attributes `attrs`
+        """
+        if attrs is None:
+            return self.__dict__
+        dict = {}
+        for key in attrs:
+            if key in self.__dict__:
+                dict.update({key: self.__dict__.get(key)})
+        return dict
+
+    def reload_from_json(self, json):
+        """ Replaces all attributes of the Student instance
+        """
+        for key in json:
+            setattr(self, key, json.get(key)) 
