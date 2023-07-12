a5ddd6340d298800e4dbf73591e7e4cb5cb4127e
Toteya <111172685+Toteya@users.noreply.github.com>
2023-07-12 11:48:34 +0000

    Task8: dictionary desciprtion for JSON, return __dict__ not json.dump

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """ Module 8-class_to_json module
 """
-import json
-
 
 def class_to_json(obj):
     """ Returns the dictionary discription for JSON
@@ -11,4 +9,4 @@ def class_to_json(obj):
     Args:
         obj: any Object whose attributes are serializable
     """
-    return json.dumps(obj.__dict__)
+    return obj.__dict__
