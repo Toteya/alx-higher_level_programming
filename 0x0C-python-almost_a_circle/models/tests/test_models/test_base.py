f2cb685cb8631c5da42fbe6315076b589752a6bd
Toteya <111172685+Toteya@users.noreply.github.com>
2023-07-13 17:56:37 +0000

    unittest file for base.py

---
+++ 0x0C-python-almost_a_circle/models/tests/test_models/test_base.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Unittest for Base class
+"""
+import unittest
+from base import Base
+
+
+class TestBase(unittest.TestCase):
+    """ Tests the Base class
+    """
+
+    def test_Base(self):
+        """ tests the Base class and instance methods
+        """
+        b0 = Base()
+        self.assertEqual(b0.id, 1)
+
+        b1 = Base()
+        self.assertEqual(b1.id, 2)
+        
+        b2 = Base(63)
+        self.assertEqual(b2.id, 63)
+        
+        b3 = Base()
+        self.assertEqual(b3.id, 3)
+
+        self.assertTrue(isinstance(b3, Base))
