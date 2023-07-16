b19a935f5d22b690d0f2ffde30d3905af36bbd58
Toteya <111172685+Toteya@users.noreply.github.com>
2023-07-16 14:44:41 +0000

    removed 0x0B

--- 0x0B-python-input_output/2-append_write.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-""" Module 2-append_write """
-
-
-def append_write(filename="", text=""):
-    """ Appends text to a file
-    """
-    with open(filename, "a", encoding="utf-8") as a_file:
-        return a_file.write(text)
