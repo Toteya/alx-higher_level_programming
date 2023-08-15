c129d2851ff1a7efc64d2c7e21b8eab6300830df
Toteya <111172685+Toteya@users.noreply.github.com>
2023-08-15 23:46:02 +0200

    list rows in descending order score, fixed column order

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,4 +1,4 @@
 -- Lists all the records of the table second_table (name and score)
 -- in descending order according to the score
-SELECT name, score FROM second_table
+SELECT score, name FROM second_table
 ORDER BY score DESC
