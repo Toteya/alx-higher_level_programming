2f21b5b35063d7bc7f261d96146341b20b8b5d9c
Toteya <111172685+Toteya@users.noreply.github.com>
2023-08-16 01:22:25 +0200

    Convert database, table, and specific column to UTF8: Correction

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -5,10 +5,8 @@ COLLATE = utf8mb4_unicode_ci;
 -- Convert the table first_table to utf8mb4
 USE hbtn_0c_0;
 ALTER TABLE first_table
-CONVERT TO CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 -- Convert the field 'name' in the table first_table to utf8mb4
 ALTER TABLE first_table
 MODIFY name VARCHAR(256)
-CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
