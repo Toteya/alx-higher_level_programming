9ea4378f61038a7b3e55c9b2b3c3cb0fceff64d6
Toteya <111172685+Toteya@users.noreply.github.com>
2023-08-15 23:44:29 +0200

    Comments

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,9 +1,10 @@
--- Creates a table 'second_table' in a database and adds multiple rows
+-- Creates a table 'second_table' in a database
 CREATE TABLE IF NOT EXISTS second_table (
 	id INT,
 	name VARCHAR(256),
 	score INT
 );
+-- Add multiple records to the table
 INSERT INTO second_table(id, name, score)
 VALUES
 (1, 'John', 10),
