15c7004facbb0e87991385b56cc3d4f7d9f428fc
Toteya <111172685+Toteya@users.noreply.github.com>
2023-08-17 22:00:22 +0200

    Fixed: incorrect listing query

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,5 @@
 -- List all the cities of California in the database 'hbtn_0d_usa'
-SELECT cities.id, states.id, cities.name
+SELECT cities.id, cities.name
   FROM cities, states
- WHERE cities.state_id = states.id;
+ WHERE cities.state_id = states.id
+   AND states.name = 'California';
