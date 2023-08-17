8d8cfd20b7184febb4736b19f41158400bc3677d
Toteya <111172685+Toteya@users.noreply.github.com>
2023-08-17 19:51:14 +0200

    correction

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,4 @@
 -- Create a new user on the MySQL server
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 -- Grant all priveleges to the new user
-GRANT ALL
-   ON *.*
-   TO 'user_0d_1'@'localhost'
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
