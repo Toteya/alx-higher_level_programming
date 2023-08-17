f03c436f6b8b32d0d805fd4d85dee1e2600d18c0
Toteya <111172685+Toteya@users.noreply.github.com>
2023-08-17 23:03:52 +0200

    list infor from two tables

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,7 @@
+-- List all shows contained in the 'hbtn_0d_tvshows' database
+-- (passed as an argument) that have atleast one genre linked
+SELECT tv_shows.title, tv_show_genres.genre_id
+  FROM tv_shows, tv_show_genres
+ WHERE tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.show_id ASC
+-- WHERE tv_shows.id = tv_show_genres.show_id;
