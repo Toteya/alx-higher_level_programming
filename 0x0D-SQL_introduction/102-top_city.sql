dd2258359434f38faf6550c27e74bdb006f71e8e
Toteya <111172685+Toteya@users.noreply.github.com>
2023-08-18 03:04:41 +0200

    List the top 3 cities' temperatures

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,7 @@
+-- List the top 3 cities temperature during July and August
+SELECT city, AVG(value) as 'avg_temp'
+  FROM temperatures
+ WHERE month BETWEEN 7 AND 8 
+ GROUP BY city
+ ORDER BY avg_temp DESC
+ LIMIT 3;
