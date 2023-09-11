c3c19e4faef92aaf500f40487a1a77a117a1e7d3
Toteya <111172685+Toteya@users.noreply.github.com>
2023-09-11 20:50:45 +0200

    Convert string to a number

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const process = require('process');
+const myInt = Number(process.argv[2]);
+if (isNaN(myInt)) {
+  console.log('Not a number');
+} else {
+  console.log(parseInt(myInt));
+}
