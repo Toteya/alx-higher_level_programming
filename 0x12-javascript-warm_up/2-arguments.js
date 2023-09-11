7e237498d2a9ddcbf8a5bd9a43dc0fe5a20b5033
Toteya <111172685+Toteya@users.noreply.github.com>
2023-09-11 19:49:13 +0200

    Conditions and command-line args

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// import { argv } from 'node:process';
+const process = require('process');
+
+if (process.argv.length < 3) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
