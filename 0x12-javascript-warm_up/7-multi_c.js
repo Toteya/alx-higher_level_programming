503e954399fef4149e58020726d4955c88ef078f
Toteya <111172685+Toteya@users.noreply.github.com>
2023-09-11 21:40:22 +0200

    Define number of iteration from command line arguments

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const process = require('process');
+
+if (isNaN(Number(process.argv[2]))) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
+    console.log('C is fun');
+  }
+}
