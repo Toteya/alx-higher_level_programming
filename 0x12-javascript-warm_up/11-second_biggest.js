b1aef87b311519085b0b879eb7f2ec4afd4f9b39
Toteya <111172685+Toteya@users.noreply.github.com>
2023-09-11 22:51:34 +0200

    Find the second largest number in an array

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+function findSecondLargest (arr) {
+  let largest = arr[0];
+  let secondLargest;
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > largest) {
+      largest = arr[i];
+    } else if (arr[i] > secondLargest || secondLargest === undefined) {
+      secondLargest = arr[i];
+    }
+  }
+  if (secondLargest === undefined) {
+    return 0;
+  }
+  return secondLargest;
+}
+
+const process = require('process');
+const myArray = process.argv.slice(2);
+for (const i in myArray) {
+  myArray[i] = parseInt(myArray[i]);
+}
+console.log(myArray);
+console.log(findSecondLargest(myArray));
