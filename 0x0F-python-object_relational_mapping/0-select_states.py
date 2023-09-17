5b8cd878da15e3a8db94d204663487dd64d8482f
Toteya <111172685+Toteya@users.noreply.github.com>
2023-09-17 14:43:16 +0200

    query a db

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,12 +5,15 @@ import sys
 
 
 def main():
-    user = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    db = MySQLdb.connect("localhost", user, password, database)
+    my_host = "localhost"
+    my_user = sys.argv[1]
+    my_pass = sys.argv[2]
+    my_database = sys.argv[3]
+
+    db = MySQLdb.connect(host=my_host, user=my_user,
+                         passwd=my_pass, db=my_database)
     cur = db.cursor()
-    cur.execute("SELECT * FROM hbtn_0e_0_usa.states")
+    cur.execute("SELECT * FROM hbtn_0e_0_usa.states ORDER BY states.id ASC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
