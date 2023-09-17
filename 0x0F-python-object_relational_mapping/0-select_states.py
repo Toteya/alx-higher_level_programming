1e23ce77f6d05f882a6e92c5202ad30941f9789e
Toteya <111172685+Toteya@users.noreply.github.com>
2023-09-17 15:29:28 +0200

    Task0: specify the port 3306

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -13,12 +13,13 @@ def main():
     command-line arguments
     """
     my_host = "localhost"
+    my_port = 3306
     my_user = sys.argv[1]
     my_pass = sys.argv[2]
     my_database = sys.argv[3]
 
     db = MySQLdb.connect(host=my_host, user=my_user,
-                         passwd=my_pass, db=my_database)
+                         passwd=my_pass, db=my_database, port=my_port)
     cur = db.cursor()
     cur.execute("SELECT * FROM hbtn_0e_0_usa.states ORDER BY states.id ASC")
     rows = cur.fetchall()
