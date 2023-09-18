af263ba9767736ddd90983a838febff13f7b7e1e
Toteya <111172685+Toteya@users.noreply.github.com>
2023-09-18 16:24:36 +0200

    filter query

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Module 1-filter_states
+Connects to and queries a db
+"""
+
+import MySQLdb
+import sys
+
+
+def main():
+    """ main function: connects to and queries the db as provided by the
+    command-line arguments
+    """
+    my_host = "localhost"
+    my_port = 3306
+    my_user = sys.argv[1]
+    my_pass = sys.argv[2]
+    my_database = sys.argv[3]
+
+    db = MySQLdb.connect(host=my_host, user=my_user,
+                         passwd=my_pass, db=my_database, port=my_port)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+
+if __name__ == '__main__':
+    main()
