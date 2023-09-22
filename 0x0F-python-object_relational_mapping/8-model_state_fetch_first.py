0933e2dd0a70bd0bec120a73da94b6ef12559067
Toteya <111172685+Toteya@users.noreply.github.com>
2023-09-22 23:06:40 +0200

    Print first object from database

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Module model_state_fetch_all
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine, select, MetaData
+import urllib
+
+
+def main():
+    """
+    Connects to and queries the given database
+    Lists all State objects
+    """
+    user = sys.argv[1]
+    password = urllib.parse.quote_plus(sys.argv[2])
+    database = sys.argv[3]
+    db_url = 'mysql+mysqldb://{}:{}@localhost/{}'.format(user,
+                                                         password,
+                                                         database)
+    engine = create_engine(db_url, pool_pre_ping=True)
+    # Base.metadata.create_all(engine)
+
+    session = engine.connect()
+    result = session.execute(select(State).order_by(State.id))
+
+    result = list(result)
+    print('{}: {}'.format(result[0].id, result[0].name))
+
+
+if __name__ == '__main__':
+    main()
