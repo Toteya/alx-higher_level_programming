bf1574b88299c7b1f0bc41e948739023cc5eed56
Toteya <111172685+Toteya@users.noreply.github.com>
2023-09-28 13:11:42 -0700

    Send an http JSON POST request - fixed error in script

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends an http JSON POST request to a URL
-curl -d @"$2" -H "Content-Type: application/json" "$1" 
+curl -sX POST -d @"$2" -H "Content-Type: application/json" "$1" 
