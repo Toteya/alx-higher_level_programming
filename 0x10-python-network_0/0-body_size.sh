9c33cd8e6c45d446f75c89ff4a5ad4acf5d910eb
Toteya <111172685+Toteya@users.noreply.github.com>
2023-09-28 09:03:21 -0700

    fixed requirement check - 3 lines

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# This script takes in a URL, sends a request to that URL, and displays the
-# size of the body of the response
-
+# This script sends a request to a URL and displays the size of the body of the http response
 curl -sI "$1" | grep 'Content-Length' | cut -d ' ' -f 2
