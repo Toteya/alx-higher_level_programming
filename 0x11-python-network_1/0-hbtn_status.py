6cc3d1dd3db2713648be773cb89315a0ecc9954f
Toteya <111172685+Toteya@users.noreply.github.com>
2023-09-29 11:24:03 +0200

    fetch a URL

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Module: 0-hbtn_status
+Fetches http://alx-intranet.hbtn.io/status
+"""
+import urllib.request
+
+
+def main():
+    url = "https://alx-intranet.hbtn.io/status"
+
+    with urllib.request.urlopen(url) as response:
+        page = response.read()
+        content = page.decode("utf-8")
+        print("\t- type: {}".format(type(page)))
+        print("\t- content: {}".format(page))
+        print("\t- utf8 content: {}".format(content))
+
+
+if __name__ == '__main__':
+    main()
