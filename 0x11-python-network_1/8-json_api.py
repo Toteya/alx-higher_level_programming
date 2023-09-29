a02788efe3968c88e9606594db2a4fa72b4f24bd
Toteya <111172685+Toteya@users.noreply.github.com>
2023-09-29 11:02:40 -0700

    request JSON response content

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -9,8 +9,17 @@ import sys
 
 if __name__ == '__main__':
     url = "http://0.0.0.0:5000/search_user"
-    letter = sys.argv[1]
-    payload = {'q': letter}
+    try:
+        payload = {'q': sys.argv[1]}
+    except IndexError:
+        payload = None
 
-    response = requests.post(url, data=values)
-    print(response.json())
+    response = requests.post(url, data=payload)
+    try:
+        body_json = response.json()
+        if not body_json:
+            print("No result")
+        else:
+            print("[{}] {}".format(body_json.get('id'), body_json.get('name')))
+    except requests.exceptions.JSONDecodeError:
+        print("Not a valid JSON")
