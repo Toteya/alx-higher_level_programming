937d0cda022e2aeaa9b382a78020f28a1bbb049c
Toteya <111172685+Toteya@users.noreply.github.com>
2023-10-01 14:49:52 +0200

    filter results by substring

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Module 9-model_state_filter_a
+"""
+
+import sys
+from model_state import State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import urllib
+
+
+def main():
+    """
+    Connects to and queries the given database
+    Lists all State objects containing 'a'
+    """
+    user = sys.argv[1]
+    password = urllib.parse.quote_plus(sys.argv[2])
+    database = sys.argv[3]
+    db_url = 'mysql+mysqldb://{}:{}@localhost/{}'.format(user,
+                                                         password,
+                                                         database)
+    engine = create_engine(db_url, pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+
+    with Session() as session:
+        result = session.query(State).filter(State.name.contains('a'))
+        # print(type(result))
+        for item in result:
+            print('{}: {}'.format(item.id, item.name))
+
+
+if __name__ == '__main__':
+    main()
