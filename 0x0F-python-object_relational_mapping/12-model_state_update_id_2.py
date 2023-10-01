b41025a81c005f399e15409c5017e9edb3a70762
Toteya <111172685+Toteya@users.noreply.github.com>
2023-10-01 18:16:22 +0200

    update a record

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Module 12-model_state_update_id_2
+"""
+
+import sys
+from model_state import State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from urllib.parse import quote
+
+
+def main():
+    """
+    Connects to and queries the given database
+    Updates the name of a State whose id is 2
+    """
+    user = sys.argv[1]
+    password = quote(sys.argv[2])
+    database = sys.argv[3]
+    db_url = 'mysql+mysqldb://%s:%s@localhost/%s' % (user,
+                                                     password,
+                                                     database)
+    engine = create_engine(db_url, pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+
+    with Session() as session:
+        with session.begin():
+            session.query(State).filter(State.id == 2).update(
+                    {
+                        'name': 'New Mexico'
+                    })
+            session.commit()
+
+
+if __name__ == '__main__':
+    main()
