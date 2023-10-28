95a79539506203c1b55632923d9bd647145194f2
Toteya <111172685+Toteya@users.noreply.github.com>
2023-10-28 15:01:52 +0200

    list all object from database

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+module 101-relationship_states_cities_list
+Lists all the State objects and the corresponding City objects contained
+in the given database
+"""
+from relationship_city import City
+from relationship_state import State
+from urllib.parse import quote
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+if __name__ == '__main__':
+    user = sys.argv[1]
+    password = quote(sys.argv[2])
+    database = sys.argv[3]
+    db_url = 'mysql+mysqldb://%s:%s@localhost/%s' % (user,
+                                                     password,
+                                                     database)
+    engine = create_engine(db_url, pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+
+    with Session() as session:
+        result = session.query(State).order_by(State.id).all()
+        for state in result:
+            print("{}: {}".format(state.id, state.name))
+            for city in state.cities:
+                print("\t{}: {}".format(city.id, city.name))
