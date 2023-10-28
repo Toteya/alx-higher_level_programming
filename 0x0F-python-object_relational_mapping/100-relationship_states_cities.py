69f930ef92e459eb7b406ca430d66f0c5e6f6b0e
Toteya <111172685+Toteya@users.noreply.github.com>
2023-10-28 13:43:41 +0200

    relationship between cities and states tables

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+module 100-relationship_state_cities.py
+Creates the State 'California' with the City 'San Fransisco' from the
+given database
+"""
+from relationship_city import City
+from relationship_state import Base, State
+from urllib.parse import quote
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+if __name__ == '__main__':
+    user = sys.argv[1]
+    password = quote(sys.argv[2])
+    database = sys.argv[3]
+    db_url = 'mysql+mysqldb://%s:%s@localhost/%s' % (user,
+                                                     password,
+                                                     database)
+    engine = create_engine(db_url, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    
+    my_city = City(name="San Francisco")
+    my_state = State(name='California', cities=[my_city])
+
+    with Session() as session:
+        session.add(my_state)
+        session.add(my_city)
+        session.commit()
---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+module model_city
+"""
+
+from relationship_state import Base
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+
+class City(Base):
+    """
+    A City
+    """
+    __tablename__ = "cities"
+    id = Column('id', Integer, unique=True, autoincrement=True, nullable=False,
+                primary_key=True)
+    name = Column('name', String(128), nullable=False)
+    state_id = Column('state_id', Integer, ForeignKey('states.id'),
+                      nullable=False)
+    state = relationship("State", back_populates="cities")
