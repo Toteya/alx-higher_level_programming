7c904c1fb3454a0b41e214ef56eff39f16c25ac5
Toteya <111172685+Toteya@users.noreply.github.com>
2023-11-01 10:56:02 +0200

    write content to a file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const filepath = process.argv[2];
+const data = process.argv[3];
+
+fs.writeFile(filepath, data, err => {
+  if (err) {
+    console.error(err);
+  }
+});
