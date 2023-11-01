f4c4cd744698c5bbabe3b26031e80f2760155c9e
Toteya <111172685+Toteya@users.noreply.github.com>
2023-11-01 12:15:25 +0200

    read JSON data from response

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const id = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + id;
+
+request(url, (error, response, body) => {
+  if (error) console.error(error);
+  const details = JSON.parse(body);
+  console.log(details.title);
+});
