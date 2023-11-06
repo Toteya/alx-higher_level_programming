5cb6db9d5bff438bd46aea1d9f1e76f79df236b1
Toteya <111172685+Toteya@users.noreply.github.com>
2023-11-06 12:00:48 +0200

    fixed: pass url as argument

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,7 +3,7 @@
 
 const request = require('request');
 
-const url = 'https://jsonplaceholder.typicode.com/todos';
+const url = process.argv[2];
 
 request(url, (error, response, body) => {
   if (error) console.error(error);
