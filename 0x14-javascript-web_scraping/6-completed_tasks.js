175033650400be6e477c5519005e87fcb698a001
Toteya <111172685+Toteya@users.noreply.github.com>
2023-11-06 11:55:25 +0200

    print completed tasks from API

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+// Computes the number of tasks completed bu user id
+
+const request = require('request');
+
+const url = 'https://jsonplaceholder.typicode.com/todos';
+
+request(url, (error, response, body) => {
+  if (error) console.error(error);
+  const todos = JSON.parse(body);
+  let id = 0;
+  let tasksCompleted = 0;
+  const dict = {};
+
+  for (const todo of todos) {
+    if (todo.userId !== id) {
+      if (tasksCompleted !== 0) dict[id.toString()] = tasksCompleted;
+      id = todo.userId;
+      tasksCompleted = 0;
+    }
+    if (todo.completed) {
+      tasksCompleted++;
+    }
+  }
+  if (tasksCompleted !== 0) dict[id.toString()] = tasksCompleted;
+  console.log(dict);
+});
